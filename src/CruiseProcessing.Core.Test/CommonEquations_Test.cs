775fb1d7d7eb52afdbe7612e1d10c6012806e153
BENJAMIN J. CAMPBELL <benjamin.campbell@usda.gov>
2025-01-24 13:13:40 -0600

    Update CommonEquations_Test.cs

--- src/CruiseProcessing.Core.Test/CommonEquations_Test.cs
+++ src/CruiseProcessing.Core.Test/CommonEquations_Test.cs
@@ -70,5 +70,46 @@ namespace CruiseProcessing.Test
             var value = (int)(input + .49);
             value.Should().Be(expected);
         }
+
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0.09, 0)]
+        [InlineData(0.1, 0)]
+        [InlineData(0.48, 0)]
+        [InlineData(0.49, 0)]
+        [InlineData(0.5, 1)]
+        [InlineData(0.51, 1)]
+        [InlineData(1.1, 1)]
+        [InlineData(1.4, 1)]
+        [InlineData(1.48, 1)]
+        [InlineData(1.49, 1)]
+        [InlineData(1.5, 2)]
+        [InlineData(1.51, 2)]
+        public void TestPoint51Rounding(double input, int expected)
+        {
+            var value = (int)Math.Floor(input + .51);
+            value.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0.09, 0)]
+        [InlineData(0.1, 0)]
+        [InlineData(0.48, 0)]
+        [InlineData(0.49, 0)]
+        [InlineData(0.5, 1)]
+        [InlineData(0.51, 1)]
+        [InlineData(1.1, 1)]
+        [InlineData(1.4, 1)]
+        [InlineData(1.48, 1)]
+        [InlineData(1.49, 1)]
+        [InlineData(1.5, 2)]
+        [InlineData(1.51, 2)]
+        public void TestAwayFromZeroRounding(double input, int expected)
+        {
+            var value = (int)Math.Round(input, MidpointRounding.AwayFromZero);
+            value.Should().Be(expected);
+        }
     }
 }
