92a046262f377794631fd9fd11e41cfc95fd91eb
BENJAMIN J. CAMPBELL <benjamin.campbell@usda.gov>
2025-03-07 16:06:23 -0600

    WIP adding helper method for finding test files

---
+++ src/CruiseProcessing.Test.Shared/TestFilesHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruiseProcessing.Test.Shared
+{
+    public class TestFilesHelper
+    {
+        public static string TestExecutionDirectory
+        {
+            get
+            {
+                var codeBase = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+                return Path.GetDirectoryName(codeBase);
+            }
+        }
+
+        public static string TestFilesDirectory => Path.Combine(TestExecutionDirectory, "TestFiles");
+
+
+        public static IEnumerable<string[]> GetTestFileNames()
+        {
+            var fileExtentions = new[] { ".crz3", ".cruise" };
+
+            var testFileDir = TestFilesDirectory;
+            var testCruiseFiles = Directory.EnumerateFiles(TestFilesDirectory, "*.*", SearchOption.AllDirectories)
+                .Where(x => fileExtentions.Contains(Path.GetExtension(x)));
+
+            foreach (var file in testCruiseFiles)
+            {
+                var fileName = Path.GetFileName(file);
+                var directory = Path.GetDirectoryName(file);
+                var outFileName = fileName + ".out";
+
+                var outPath = Path.Combine(directory, outFileName);
+                if (File.Exists(outPath))
+                {
+                    var filePathShort = file.Remove(0, testFileDir.Length);
+                    var outPathShort = outPath.Remove(0, testFileDir.Length);
+
+                    yield return new[] { filePathShort, outPathShort };
+                }
+            }
+        }
+    }
+}
