f03132fe96e5c2d10835934eaf8aefabf16b2ab3
BENJAMIN J. CAMPBELL <benjamin.campbell@usda.gov>
2025-04-10 15:27:02 -0500

    move Ng processing models to Interop project

--- src/CruiseProcessing.Processing.JsInterop/Models/NgLogInfo.cs
+++ src/CruiseProcessing.Processing.JsInterop/Models/NgLogInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.JavaScript.NodeApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace CruiseProcessing.Processing.Models
 {
+    [JSExport]
     public class NgLogInfo
     {
         public string TreeID { get; set; }
